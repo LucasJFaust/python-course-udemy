3ba190bc6291cbf1dc1bd9f297a789e6413198ed
LucasJFaust <lucasjfm.eng@gmail.com>
2025-08-26 13:11:49 -0300

    Resolvendo os exercicios

--- strings/8-desafios-string.py
+++ strings/8-desafios-string.py
@@ -103,3 +103,11 @@ misturar latas e galões, de forma que o desperdício de tinta seja menor. Acres
 09 - Faça um programa que peça o tamanho de um arquivo para download (em MB) e a velocidade de um link de Internet
 (em Mbps), calcule e informe o tempo aproximado de download do arquivo usando este link (em minutos).
 """
+tamanho_arquivo_mb = float(input("Digite o tamanho do arquivo para download (em MB): "))
+velocidade_link_mbps = float(input("Digite a velocidade do link de Internet (em Mbps): "))
+
+# Convertendo a velocidade de Mbps para MBps (1 byte = 8 bits)
+velocidade_link_mbps_to_mbps = velocidade_link_mbps / 8 # em MBps
+tempo_download_segundos = tamanho_arquivo_mb / velocidade_link_mbps_to_mbps
+tempo_download_minutos = tempo_download_segundos / 60
+print(f"O tempo aproximado de download do arquivo é: {tempo_download_minutos:.2f} minutos")
